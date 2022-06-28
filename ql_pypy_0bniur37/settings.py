f8d8bc208027a2f9be812ebe70331faa6bbb54de
ldy827 <103937790+ldy827@users.noreply.github.com>
2022-06-28 15:01:48 +0900

    Update settings.py

--- ql_pypy_0bniur37/settings.py
+++ ql_pypy_0bniur37/settings.py
@@ -26,7 +26,8 @@ SECRET_KEY = 'django-insecure-)bg(4ofhzn9i(n@7z*d06cbxif-dkhkngs*z8$w&n&&6)jl@s6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['100.64.2.0/24']
+ALLOWED_HOSTS = ['localhost']
+ALLOWED_CIDR_NETS = ['100.64.2.0/24']
 
 
 # Application definition
